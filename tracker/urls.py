aeacd0355c2bb146d5a48aceb5a06060d0cf3602
brickfox <brickfox@brickFox.lan>
2025-09-14 00:11:07 +0300

    fix: fixed project creation

--- tracker/urls.py
+++ tracker/urls.py
@@ -18,14 +18,19 @@ urlpatterns = [
         name="leave_member"
     ),
     path("invitation/send/<int:group_id>", views.send_invitation, name='send_invitation'),
-    #path("members/<int:pk>/profile/", views.view_profile, name="view_profile"),
-    #path("members/<int:pk>/delete/", views.delete_member, name="delete_member"),
     path("projects/", view=views.project_list , name="project_list"),
-    path("projects/create", view=views.create_project, name="create_project"),
+    path("projects/create/<int:group_id>/", views.create_project, name="create_project"),
     path("projects/<int:project_id>/", view=views.project_details, name="project_details"),
     path("projects/<int:project_id>/create-ticket", view=views.create_ticket, name="create_ticket"),
     path("tickets/<int:project_id>/", view=views.ticket_detail, name="ticket_detail"),
-    path("tickets/<int:project_id>/update", view=views.update_ticket, name="update_ticket"),
-
-    path("api/autocomplete/emails/", view=views.user_email_autocomplete, name="user_email_autocomplete"),
+    path(
+        "tickets/<int:project_id>/update", 
+        view=views.update_ticket, 
+        name="update_ticket"
+    ),
+    path(
+        "api/autocomplete/emails/", 
+        view=views.user_email_autocomplete, 
+        name="user_email_autocomplete"
+    ),
 ]   
